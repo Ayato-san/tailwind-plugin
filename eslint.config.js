9d6c0412e9dfdde8ca7bb588d50a239db6f32795
Ayato <ayatosan.pro@gmail.com>
2024-09-17 13:20:49 +0200

    chore: update lint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,3 @@
 import { configure } from '@ayato-san/tooling-configs/eslint'
 
-export default configure(undefined, {
-  name: 'Overides',
-  rules: {
-    '@typescript-eslint/no-unsafe-assignment': 'off',
-  },
-})
+export default configure()
